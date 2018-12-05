ca19a563a1ddaf38fa0d894ba7a6a82d404a59d2
dengchao1302 <deng.chao1302@gmail.com>
2018-12-05 14:10:33 +0700

    Truy xuất database

---
+++ Back-end/DataProvider.php
@@ -0,0 +1,23 @@
+<?php
+	define("SERVER","localhost");
+	define("DATABASE","QLBanHang");
+	define("USERNAME","root");
+	define("PASSWORD","");
+
+	class DataProvider
+	{
+		public static function execQuery($sql)
+		{
+			$connection = mysqli_connect(SERVER,USERNAME,PASSWORD,DATABASE) or die("Could not connect to " .SERVER . ".");
+			
+			mysqli_select_db( $connection,DATABASE);
+			mysqli_set_charset($connection,"utf8");
+			
+			$result = mysqli_query($connection,$sql);
+			if(!$result) die("Query failed: " . mysqli_error($connection));
+			
+			mysqli_close($connection);
+			return $result;
+		}
+	}
+?>
