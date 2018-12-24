27528edbc795360df094396773fbb11cea771920
ptquochuy1612 <ptqhuy1612@gmail.com>
2018-12-24 19:46:33 +0700

    80% homepage

--- Back-end/1.1.1.php
+++ Back-end/1.1.1.php
@@ -2,7 +2,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Hien thi 10 san pham moi nhat</title>
+<title>My Shop</title>
 </head>
 <!-- 10 sản phẩm mới nhất -->
 
